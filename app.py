0744c251b51098d24d6d804398c99decf1030796
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-01-28 14:05:37 -0700

    guide was deleted

--- app.py
+++ app.py
@@ -80,7 +80,10 @@ def guide_delete(id):
     db.session.delete(guide)
     db.session.commit()
 
-    return guide_schema.jsonify(guide)
+    return "Guide was successfully deleted!"
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
